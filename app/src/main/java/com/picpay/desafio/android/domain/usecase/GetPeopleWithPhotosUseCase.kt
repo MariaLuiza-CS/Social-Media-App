f38d2654e0c9f091337ec7f2063245c0e39e809e
MariaLuiza-CS <m.luiza1843@gmail.com>
2025-11-22 23:59:33 -0300

    feature: adding bootom navigation

--- app/src/main/java/com/picpay/desafio/android/domain/usecase/GetPeopleWithPhotosUseCase.kt
+++ app/src/main/java/com/picpay/desafio/android/domain/usecase/GetPeopleWithPhotosUseCase.kt
@@ -1,14 +1,14 @@
 package com.picpay.desafio.android.domain.usecase
 
-import com.picpay.desafio.android.data.local.entity.PersonWithPhotosEntity
 import com.picpay.desafio.android.data.repository.PeopleRepository
+import com.picpay.desafio.android.domain.model.PersonWithPhotos
 import com.picpay.desafio.android.domain.model.Result
 import kotlinx.coroutines.flow.Flow
 
 class GetPeopleWithPhotosUseCase(
     private val peopleRepository: PeopleRepository
 ) {
-    operator fun invoke(): Flow<Result<List<PersonWithPhotosEntity?>>> {
+    operator fun invoke(): Flow<Result<List<PersonWithPhotos?>>> {
         return peopleRepository.getPeople()
     }
 }
